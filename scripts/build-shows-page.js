0a05d2f9287769dc8fa658164a7e3c1e464e6164
Gillian Trethewey <gilliantrethewey100@gmail.com>
2024-01-04 23:27:52 -0800

    improves shows data to be shown as a table

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,107 +1,115 @@
 let shows = [
-    {
-      date: "Mon Sept 06 2021",
-      venue: "Ronald Lane",
-      location: "San Francisco, CA",
-    },
-  
-    {
-      date: "Tue Sept 21 2021",
-      venue: "Pier 3 East",
-      location: "San Francisco, CA",
-    },
-  
-    {
-      date: "Fri Oct 15 2021",
-      venue: "View Lounge",
-      location: "San Francisco, CA",
-    },
-  
-    {
-      date: "Sat Nov 06 2021",
-      venue: "Hyatt Agency",
-      location: "San Francisco, CA",
-    },
-  
-    {
-      date: "Fri Nov 26 2021",
-      venue: "Moscow Center",
-      location: "San Francisco, CA",
-    },
-  
-    {
-      date: "Wed Dec 15 2021",
-      venue: "Press Club",
-      location: "San Francisco, CA",
-    },
-  ];
-  
-  let showsTable = document.querySelector(".shows__table");
-  
-  // header that displays in tablet and above
-  let tabletHeader = document.createElement("div");
-  tabletHeader.className = "tablet__header";
-  
-  let headerDate = document.createElement("p");
-  headerDate.innerText = "DATES";
-  headerDate.className = "tablet__date";
-  tabletHeader.appendChild(headerDate);
-  
-  let headerVenue = document.createElement("p");
-  headerVenue.innerText = "VENUE";
-  headerVenue.className = "tablet__venue";
-  tabletHeader.appendChild(headerVenue);
-  
-  let headerLocation = document.createElement("p");
-  headerLocation.innerText = "LOCATION";
-  headerLocation.className = "tablet__location";
-  tabletHeader.appendChild(headerLocation);
-  
-  showsTable.appendChild(tabletHeader);
-  
+  {
+    date: "Mon Sept 06 2021",
+    venue: "Ronald Lane",
+    location: "San Francisco, CA",
+  },
+
+  {
+    date: "Tue Sept 21 2021",
+    venue: "Pier 3 East",
+    location: "San Francisco, CA",
+  },
+
+  {
+    date: "Fri Oct 15 2021",
+    venue: "View Lounge",
+    location: "San Francisco, CA",
+  },
+
+  {
+    date: "Sat Nov 06 2021",
+    venue: "Hyatt Agency",
+    location: "San Francisco, CA",
+  },
+
+  {
+    date: "Fri Nov 26 2021",
+    venue: "Moscow Center",
+    location: "San Francisco, CA",
+  },
+
+  {
+    date: "Wed Dec 15 2021",
+    venue: "Press Club",
+    location: "San Francisco, CA",
+  },
+];
+let table = document.querySelector(".table");
+let tableBody = document.querySelector(".table__body");
+
+// // header that displays in tablet and above
+// let tabletHead = document.createElement("thead");
+// tabletHead.className = "tablet__head";
+
+// let tabletRowTop = document.createElement("tr");
+// tabletRowTop.className = "tablet__row-top";
+// tabletHead.appendChild(tabletRowTop);
+
+// let headDate = document.createElement("td");
+// headDate.innerText = "DATE";
+// headDate.className = "tablet__date";
+// tabletHead.appendChild(headDate);
+
+// let headVenue = document.createElement("td");
+// headVenue.innerText = "VENUE";
+// headVenue.className = "tablet__venue";
+// tabletHead.appendChild(headVenue);
+
+// let headLocation = document.createElement("td");
+// headLocation.innerText = "LOCATION";
+// headLocation.className = "tablet__location";
+// tabletHead.appendChild(headLocation);
+
+// let headButtonHidden = document.createElement("td");
+// headButtonHidden.className = "table__button--hidden";
+// tabletHead.appendChild(headButtonHidden);
+
+// table.appendChild(tabletHead);
+
 const generateTable = (data) => {
-    let showCard = document.createElement("div");
-    showCard.className = "show__card";
-  
-    let dateHeading = document.createElement("p");
-    dateHeading.innerText = "DATE";
-    dateHeading.className = "show__heading";
-    showCard.appendChild(dateHeading);
-  
-    let dateData = document.createElement("h3");
-    dateData.innerText = data["date"];
-    dateData.className = "show__date";
-    showCard.appendChild(dateData);
-  
-    let venueHeading = document.createElement("p");
-    venueHeading.innerText = "VENUE";
-    venueHeading.className = "show__heading";
-    showCard.appendChild(venueHeading);
-  
-    let venueData = document.createElement("p");
-    venueData.innerText = data["venue"];
-    venueData.className = "show__venue";
-    showCard.appendChild(venueData);
-  
-    let locationHeading = document.createElement("p");
-    locationHeading.innerText = "LOCATION";
-    locationHeading.className = "show__heading";
-    showCard.appendChild(locationHeading);
-  
-    let locationData = document.createElement("p");
-    locationData.innerText = data["location"];
-    locationData.className = "show__location";
-    showCard.appendChild(locationData);
-  
-    showsTable.appendChild(showCard);
-  
-    let buttonElement = document.createElement("button");
-    buttonElement.innerText = "BUY TICKETS";
-    buttonElement.classList.add("show__button");
-    showCard.appendChild(buttonElement);
-  }
-  
-  for (const show of shows) {
-    generateTable(show);
-  }
-  
+  let tableRow = document.createElement("tr");
+  tableRow.className = "table__row";
+
+  let dateHeading = document.createElement("td");
+  dateHeading.innerText = "DATE";
+  dateHeading.className = "table__heading";
+  tableRow.appendChild(dateHeading);
+
+  let dateData = document.createElement("td");
+  dateData.innerText = data["date"];
+  dateData.className = "table__date";
+  tableRow.appendChild(dateData);
+
+  let venueHeading = document.createElement("td");
+  venueHeading.innerText = "VENUE";
+  venueHeading.className = "table__heading";
+  tableRow.appendChild(venueHeading);
+
+  let venueData = document.createElement("td");
+  venueData.innerText = data["venue"];
+  venueData.className = "table__venue";
+  tableRow.appendChild(venueData);
+
+  let locationHeading = document.createElement("td");
+  locationHeading.innerText = "LOCATION";
+  locationHeading.className = "table__heading";
+  tableRow.appendChild(locationHeading);
+
+  let locationData = document.createElement("td");
+  locationData.innerText = data["location"];
+  locationData.className = "table__location";
+  tableRow.appendChild(locationData);
+
+  let buttonElement = document.createElement("button");
+  buttonElement.innerText = "BUY TICKETS";
+  buttonElement.classList.add("table__button");
+
+  tableRow.appendChild(buttonElement);
+
+  tableBody.appendChild(tableRow);
+};
+
+for (const show of shows) {
+  generateTable(show);
+}
